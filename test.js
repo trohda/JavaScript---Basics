0e8ac23db4b21e01d6486de844f26c5477e03a12
Tomasz Rohda <tomaszrohda@wp.pl>
2022-02-13 10:44:31 +0100

    Examples of variables

--- test.js
+++ test.js
@@ -1,1 +1,19 @@
 alert("Hello, World");
+
+var myVariableTomek; //undefined variable
+
+var myVariableTomek = 10; //defined variable
+
+    var      myVar2    =     "hello";
+//   ^         ^                ^
+//   |         |                |
+//define    variable         variable
+//variable    name             value
+
+
+//changing type of variable
+typeof myVar2; //'string"
+myVar2 = 5;
+typeof myVar2; //'number'
+
+//JS have dynamic type variables
