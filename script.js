b862972746f194ee37641fa7318d9f1570eb9774
Tomasz Rohda <tomaszrohda@wp.pl>
2022-02-14 11:02:17 +0100

    Logical operators

---
+++ script.js
@@ -0,0 +1,59 @@
+/*
+
+if(conditions){
+
+    //code block
+
+}
+
+>
+<
+=>
+=<
+==
+===
+!=
+!==
+
+*/
+
+
+
+// double apersand && (2x&)
+
+var myAge = 25;
+
+if(myAge >= 18 && myAge <= 30){
+
+    document.write("You can come in!")
+
+}else{
+
+    document.write("You can`t come in!")
+
+}
+
+var myAge = 15;
+
+if(myAge >= 18 && myAge <= 30){
+
+    document.write("You can come in!")
+
+}else{
+
+    document.write("You can`t come in!")
+
+}
+
+
+var myAge = 25;
+
+if(myAge >= 18 || myAge == 30){
+
+    document.write("You can come in!")
+
+}else{
+
+    document.write("You can`t come in!")
+
+}
