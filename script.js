b4beb3ad236b07545d9d71f4b9f447c748482bb1
Tomasz Rohda <tomaszrohda@wp.pl>
2022-02-14 22:06:44 +0100

    For loop

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
+/*
 var age = 5;
 
+//loop while
 while(age < 10){
 
     console.log("Your age is less than 10");
@@ -8,3 +10,26 @@ while(age < 10){
 }
 
 document.write("You are now over 10");
+*/
+
+//loop for
+/*
+for (age=5; age<10; age++){
+
+    console.log("Your age is less than 10");
+
+}
+
+document.write("You are now over 10");
+*/
+
+var links = document.getElementsByTagName("a");
+
+for ( i = 1; i <= links.length; i++){
+
+    console.log("This is link number" + i);
+
+}
+
+document.write("All links are looped");
+
