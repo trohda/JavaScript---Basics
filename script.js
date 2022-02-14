18f3e2e6dc8837f62a37631792c311088f8f0eae
Tomasz Rohda <tomaszrohda@wp.pl>
2022-02-14 10:32:36 +0100

    If, else Statements

---
+++ script.js
@@ -0,0 +1,54 @@
+var youLikeMeet = true;
+
+if(youLikeMeet){
+
+    document.write("Where is the meet menu...");
+
+}
+var youLikeMeet = false;
+
+if(youLikeMeet){
+
+    document.write("No meet menu...");
+
+}else{
+
+    document.write("<br>You hate me");
+}
+
+
+if(7>5){
+
+    document.write("<br>This expression is true");
+
+}
+
+var myNum=10;
+
+if(myNum > 10){
+
+    document.write("myNum is grater than ten");
+
+}
+
+if(myNum > 9){
+
+    document.write("<br>myNum is grater than nine");
+
+}
+
+if(myNum = 10){
+
+    document.write("<br>myNum is equal to ten");
+
+}
+
+if(myNum > 10){
+
+    document.write("myNum is grater than ten");
+
+}else{
+
+    document.write("<br>myNum is not grater than ten");
+
+}
