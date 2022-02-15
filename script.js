a15a47dd2d21df476983dc7e3bbc2984af1e6593
Tomasz Rohda <tomaszrohda@wp.pl>
2022-02-15 13:13:02 +0100

    Example of using loop

--- script.js
+++ script.js
@@ -1,19 +1,6 @@
+var links = document.getElementsByTagName("a");
+for(i=0; i<links.length; i++){
 
-
-for ( i = 0; i <= 10; i++){
-
-    if(i===5 || i===3){
-
-        continue;
-    }
-
-    console.log(i);
-
-    if(i===7){
-        break;
-    }
+    links[i].className = "link-" +i;
 
 }
-
-console.log("I have broken out of the loop");
-
