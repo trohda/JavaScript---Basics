3105ceb87543a336cf66d4cdfc01d7c6f897c06a
Tomasz Rohda <tomaszrohda@wp.pl>
2022-02-17 09:19:54 +0100

    Introduction to Objects

--- test.js
+++ test.js
@@ -1,37 +1,15 @@
-var myArray = []; //empty array
+/*
+var myCar = new Car(); //create new instant of a Car object - (object Car is not define) - its example
+myCar.length, myCar.topSpeed //example of property of object myCar
+myCar.drive() , myCar.break() //example of methods(functions) of object myCar
+*/
 
-myArray[0] = 25;
+var myString = new String();
+myString = "hello";
+console.log(myString.length);
+console.log(myString.toLocaleUpperCase());
 
-console.log(myArray);
-console.log(myArray[0]);
-
-myArray[1] = 35;
-myArray[2] = true;
-myArray[3] = "hello";
-
-console.log(myArray);
-console.log(myArray[0]);
-console.log(myArray[2]);
-console.log(myArray[3]);
-
-
-//update value 
-myArray[2] = false; //update position 2
-
-console.log(myArray);
-console.log(myArray[2]);
-
-//concept of new array
-var myArray2 = [10,20,"hi",false];
-console.log(myArray2);
-
-var myArray3 = new Array(); //create new instant of array object - the same result as var myArra3 = [] 
-
-var myArray4 = new Array(5); //we can specify how many things we want in array
-
-//property of arrays
-console.log(myArray2.length); //4
-
-//methods of arrays
-console.log(myArray2.sort()); //from   10, 20, 'hi', false     to     10, 20, false, 'hi'   
-console.log(myArray2.reverse());
+var myString2 = new String();
+myString2 = "hi there";
+console.log(myString2.length);
+console.log(myString2.toLocaleUpperCase());
