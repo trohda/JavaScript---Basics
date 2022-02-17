484cba56c9cf08c6eb447e70674440ad094255f0
Tomasz Rohda <tomaszrohda@wp.pl>
2022-02-17 08:50:42 +0100

    Slice and Split strings

--- test.js
+++ test.js
@@ -1,54 +1,16 @@
-/*
+//Slice method
 
-var myString = "I am a string";
-var myString1 = 'I am a "fun" string';
-var myString2 = 'I\'m a "fun" string';
+var str = "hello, world";
 
-console.log(myString);
-console.log(myString1);
-console.log(myString2);
+var str2 = str.slice(2, 9); //start at position 2 and end at 9
+console.log(str2);
 
-*/
+var str3 = str.slice(2); //start at position 2 and end at the end because end is not defined
+console.log(str3); 
 
+//Split method
 
-/*
-//property of string
+var tags = "meat, ham, salami, pork, beef, chicken";
+var tagsArray = tags.split(","); //everytime you see , add text array element
 
-var myString = "I am a string";
-console.log(myString.length);
-*/
-
-/*
-//methods of strings
-
-var myString = 'I\'m a "fun ninja" string';
-console.log(myString.toUpperCase());
-console.log(myString.toLowerCase());
-
-console.log(myString.indexOf("string"));
-console.log(myString.indexOf("ninja")); // result -1  -  doesn`t exist
-if(myString.indexOf("ninja") === -1){
-
-    console.log("The word ninja is not in the string");
-
-}else{
-
-    console.log("The word ninja start ot position: " + myString.indexOf("ninja"));
-}
-*/
-
-var string1 = "abc";
-var string2 = "efg";
-var string3 = "abc";
-var string4 = "ABC";
-var string5 = "bcd";
-var string6 = "Bcd";  
-
-console.log(string1 === string2); //false
-console.log(string1 === string3); //true
-console.log(string1 === string4); //false
-console.log(string1 == string4); //false
-console.log(string1 === string4.toLocaleLowerCase()); //true
-console.log(string1 < string5); //true -  a is before b in alphabet
-console.log(string1 < string6);; //false - CAPITAL letters are grater than non capital letter
-console.log(string1.toUpperCase() < string6); //true
+console.log(tagsArray);
